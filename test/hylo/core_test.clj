5f36645ebaad6a1a1f921f4b2a1a4350808fc433
Matthew Farrell <farrellm@alum.mit.edu>
2016-11-04 23:14:54 -0400

    add clj->ir test

--- test/hylo/core_test.clj
+++ test/hylo/core_test.clj
@@ -3,11 +3,23 @@
             [hylo.core :refer :all]
             [hylo.types :refer :all]))
 
-(deftest a-test
+(deftest ir-test
   (testing "doesn't crash"
     (doseq [exp [(e-lit :long 8)
                  (e-fn 'x (e-sym 'x))
                  (e-let 'id (e-fn 'x (e-sym 'x)) (e-sym 'id))]]
       (-> (type-inference {} exp)
-          show-type))
+          show-type
+          println))
+    (is true)))
+
+(deftest clj-test
+  (testing "doesn't crash"
+    (doseq [exp '[8
+                  (fn [x] x)
+                  (let [id (fn [x] x)] id)]]
+      (->> (clj->ir exp)
+           (type-inference {})
+           show-type
+           println))
     (is true)))
